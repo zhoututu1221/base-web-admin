a6a67fe56e8025ef99d1e52dee403213dc77f931
zhoututu <497099936@qq.com>
2021-08-21 19:24:36 +0800

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 注册后有未使用的组件将不报错
+    "vue/no-unused-components": "off"
+  }
+}
